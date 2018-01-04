955cd34ab73f73e47ff3cf08613fbcfccf1e2e35
xshen <mshen_85@hotmail.com>
2018-01-04 22:58:45 +0800

    5

--- shipping.py
+++ shipping.py
@@ -1,13 +1,27 @@
+import  iso6346
 class ShippingContainer:
     next_serial = 1337
 
+    @staticmethod
+    def _make_bic_code(owner_code, serial):
+        return iso6346.create(owner_code = owner_code, serial =str(serial).zfill(6))
     @staticmethod
     def _get_next_serial():
         result = ShippingContainer.next_serial
         ShippingContainer.next_serial += 1
         return result
 
+    @classmethod
+    def create_empty(cls, owner_code):
+        return cls(owner_code,contents= None)
+
+    @classmethod
+
     def __init__(self, owner_code, contents):
-        self.owner_code = owner_code
         self.contents = contents
-        self.serial = ShippingContainer._get_next_serial()
+        self.bic = self._make_bic_code(owner_code = owner_code, serial= ShippingContainer._get_next_serial())
+
+class RefrigeratedShippingContainer(ShippingContainer):
+    @staticmethod
+    def _make_bic_code(owner_code, serial):
+        return iso6346.create(owner_code,str(serial).zfill(6),'R')
