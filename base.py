1420b4d8987c12fd03ce2a208cbf755f5938decf
xshen <mshen_85@hotmail.com>
2018-01-13 22:17:47 +0800

    shipping

---
+++ base.py
@@ -0,0 +1,10 @@
+class Base:
+    def __init__(self):
+        print('Base Initializer')
+
+    def f(self):
+        print('Base.f()')
+
+class Sub(Base):
+    def __init__(self):
+        print('Sub Initializer')
--- example_iterator.py
+++ example_iterator.py
@@ -1,7 +1,8 @@
 class ExampleIterator:
-    def __int__(self):
+    def __int__(self,data):
         self.index = 0
-        self.data = [1,2,3]
+        self.data = data
+        print(self.index)
 
 
     def __iter__(self):
@@ -9,9 +10,13 @@ class ExampleIterator:
 
 
     def __next__(self):
-        if self.index <= len(self.data)
+        if self.index >= len(self.data):
             raise StopIteration()
 
         rslt = self.data[self.index]
-        self.index +=1
+        self.index += 1
         return rslt
+
+if __name__ == "__main__":
+        i = ExampleIterator("[1,2,3]")
+        next(i)
